bd6ff7461d696f760750b89b3ba030ca22fc174f
stef <github@stef1904berg.nl>
2023-10-10 10:59:01 +0200

    - previouswindow fix

--- Wasmachine9000/Wasmachine9000/Helpers.cs
+++ Wasmachine9000/Wasmachine9000/Helpers.cs
@@ -1,41 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using Wasmachine9000.Game;
+﻿using System.Windows;
 
 namespace Wasmachine9000
 {
     internal class Helpers
     {
-        public static void OpenWindow (Window targetWindow)
+        public static void OpenWindow(Window targetWindow)
         {
             // Store currently open window in variable
             Window currentWindow = App.GameState.CurrentWindow;
-            
+
             // Show new window and set it as current window in GameState
             targetWindow.Show();
             App.GameState.CurrentWindow = targetWindow;
-            
+
 
             // Set previous window in GameState to previously closed window and close window
             currentWindow.Hide();
-            if(App.GameState.PreviousWindow!= null)
+            if (App.GameState.PreviousWindow != null)
             {
                 App.GameState.PreviousWindow.Close();
             }
-      
+
             App.GameState.PreviousWindow = currentWindow;
-
-
-
         }
+
         public static void OpenPreviousWindow()
         {
-            OpenWindow(App.GameState.PreviousWindow);
+            Window targetWindow = App.GameState.PreviousWindow;
+            Window currentWindow = App.GameState.CurrentWindow;
+
+            targetWindow.Show();
+            currentWindow.Hide();
+
+            App.GameState.PreviousWindow = currentWindow;
+            App.GameState.CurrentWindow = targetWindow;
         }
     }
 }
