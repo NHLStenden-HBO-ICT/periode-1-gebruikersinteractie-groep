6d369a264bb731b86ea19c4de6a653044eb565f5
ItzJustLars <74597365+ItsLars03@users.noreply.github.com>
2023-10-13 23:45:39 +0300

    Gameover screen design + connection
    Gameover scherm gemaakt en de buttons verbonden met de andere schermen

---
+++ Wasmachine9000/Wasmachine9000/Windows/GameOver.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wasmachine9000.Windows
+{
+    /// <summary>
+    /// Interaction logic for GameOver.xaml
+    /// </summary>
+    public partial class GameOver : Window
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Helpers.OpenWindow(new MainWindow());
+        }
+
+        private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        {
+            Helpers.OpenWindow(new GameWindow());
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
