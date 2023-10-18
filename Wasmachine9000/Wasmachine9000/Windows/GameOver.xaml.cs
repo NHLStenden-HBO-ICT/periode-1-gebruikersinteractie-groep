4e3f5f090258eceb46ab3994b69db562812a4c61
ItzJustLars <74597365+ItsLars03@users.noreply.github.com>
2023-10-18 13:57:48 +0200

    Gameover screen add score

--- Wasmachine9000/Wasmachine9000/Windows/GameOver.xaml.cs
+++ Wasmachine9000/Wasmachine9000/Windows/GameOver.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wasmachine9000.Game;
 
 namespace Wasmachine9000.Windows
 {
@@ -29,14 +31,34 @@ namespace Wasmachine9000.Windows
             Helpers.OpenWindow(new MainWindow());
         }
 
-        private void PlayAgain_Click(object sender, RoutedEventArgs e)
-        {
-            Helpers.OpenWindow(new GameWindow());
-        }
+        //private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Helpers.OpenWindow(new GameWindow());
+        //}
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void PlayAgain_Click(object sender, EventArgs e)
+        {
+            // Get the playername from the game over screen
+            string playername = Playername.Text;
+
+            // Call the PostScore method
+            string username = playername;
+            int score = 50000;
+            App.Scoreboard.PostScore(username, score);
+
+            Console.WriteLine("Score gepost!");
+
+
+            var player = new GameWindow(); // Maak een instantie van de klasse waar _playerScore is gedefinieerd.
+            string scoreString = player._playerScore.ToString(); // Roep de ToString-methode aan.
+
+            //View score on the game over screen
+            Score.Text = scoreString;
         }
     }
 }
