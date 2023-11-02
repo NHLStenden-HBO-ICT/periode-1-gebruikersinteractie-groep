8ad640398fbc0601f5fb8148fdde3eeaf09bc13d
SilvenNorbruis <silven.norbruis@student.nhlstenden.com>
2023-11-02 13:58:41 +0100

    correctie huisstijl en bugfix leaderboard

--- Wasmachine9000/Wasmachine9000/Windows/MainWindow.xaml.cs
+++ Wasmachine9000/Wasmachine9000/Windows/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 using Wasmachine9000.Game;
 using Wasmachine9000.Windows;
 
@@ -15,6 +17,7 @@ namespace Wasmachine9000
     public partial class MainWindow : Window
     {
         private List<ScoreboardItem> _scoreboard = new();
+        public DispatcherTimer ScoreboardTimer = new DispatcherTimer();
 
         public MainWindow()
         {
@@ -26,14 +29,27 @@ namespace Wasmachine9000
 
             _scoreboard = App.Scoreboard.GetScoreboard();
             UpdateScoreboard(_scoreboard);
+            ScoreboardTimer.Interval= TimeSpan.FromSeconds(10);
+            ScoreboardTimer.Tick += ScoreboardTimer_Tick;
+            ScoreboardTimer.Start();    
+        }
+
+        private void ScoreboardTimer_Tick(object? sender, EventArgs e)
+        {
+            _scoreboard = App.Scoreboard.GetScoreboard();
+            UpdateScoreboard(_scoreboard);
         }
 
         private void UpdateScoreboard(List<ScoreboardItem> scoreboard)
         {
             int scoreIndex = 1;
 
+            // copy scorevoard
+            List<UIElement> childrenCopy = new List<UIElement>(ScoreboardContainer.Children.OfType<UIElement>());
+
+
             // Clear all items on the grid except the title text
-            foreach (UIElement child in ScoreboardContainer.Children)
+            foreach (UIElement child in childrenCopy)
             {
                 if (child is not Border) ScoreboardContainer.Children.Remove(child);
             }
